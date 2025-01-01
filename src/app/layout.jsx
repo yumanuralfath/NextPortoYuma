594f7707552819e8b313a0748837b36562b0d7de
Yuma Nur Alfath <108206424+yumanuralfath@users.noreply.github.com>
2025-01-01 09:09:25 +0700

    update: update Metadata image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,14 +12,14 @@ export const metadata = {
   description:
     "Yuma Nur Alfath Portfolio Website build with Next.js and Rocket Rust Framework",
   openGraph: {
-    title: "Yuma Nur Alfath Website",
+    title: "Yuma Nur Alfath | Portfolio Website",
     description:
       "Yuma Nur Alfath Portfolio Website build with Next.js and Rocket Rust Framework",
     url: "https://yumana.my.id",
     siteName: "Yuma Nur Alfath Website",
     images: [
       {
-        url: "https://www.yumana.my.id/hero.png",
+        url: "https://yumana.my.id/hero_metadata.png",
         width: 800,
         height: 600,
       },
@@ -29,16 +29,17 @@ export const metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Yuma Nur Alfath Website",
+    title: "Yuma Nur Alfath | Portfolio Website",
     description:
       "Yuma Nur Alfath Portfolio Website build with Next.js and Rocket Rust Framework",
-    images: ["https://www.yumana.my.id/hero.png"],
+    images: ["https://yumana.my.id/hero_metadata.png"],
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="p:domain_verify" content="c801e44222b4e6bc238e8714a4e171de" />
       <body className={inter.className}>
         <TransitionProvider> {children} </TransitionProvider>
       </body>
