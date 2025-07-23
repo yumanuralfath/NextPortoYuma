6269082a209c66decd3cabb590134a0b180fb221
yumanuralfath <yumanuralfath2@gmail.com>
2025-07-24 06:55:26 +0700

    fix(blog): remove wrong article

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
         hostname: "yumana.my.id",
       },
     ],
